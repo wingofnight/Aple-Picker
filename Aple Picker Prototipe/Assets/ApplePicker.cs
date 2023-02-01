58ba54adb157938d7ee87d5b99fb423d52149066
Владимир <51334865+wingofnight@users.noreply.github.com>
2023-02-01 04:44:55 +0300

    1

---
+++ Aple Picker Prototipe/Assets/ApplePicker.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ApplePicker : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBoomY = -14f;
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+    void Start()
+    {
+        basketList = new List<GameObject>();
+
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBoomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
+        }
+    }
+    public void AppleDestroyed()
+    {
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
